b0b6cac812d27ea1fd1f24ff421e3950ad4e3be1
Luis Lopez <luis.dev.master@gmail.com>
2023-02-13 02:35:45 -0600

    Implementing User controller features

--- src/main/java/com/example/myapp/controller/UserController.java
+++ src/main/java/com/example/myapp/controller/UserController.java
@@ -1,4 +1,42 @@
 package com.example.myapp.controller;
 
+import com.example.myapp.entity.User;
+import com.example.myapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("user")
 public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+    @GetMapping("{id}")
+    public User findById(@PathVariable int id){
+        return userService.findById(id);
+    }
+    @PostMapping
+    public User save(@RequestBody User user){
+        userService.saveOrUpdate(user);
+        return user;
+    }
+    @PutMapping
+    public User update(@RequestBody User user){
+        userService.saveOrUpdate(user);
+        return user;
+    }
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable int id){
+        userService.delete(id);
+        return "User deleted";
+    }
 }
