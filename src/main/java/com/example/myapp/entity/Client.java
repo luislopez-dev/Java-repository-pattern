382462d17c58943806fb360db0b70d76e076ab75
Luis Lopez <luis.dev.master@gmail.com>
2023-02-12 22:11:58 -0600

    Creating Client entity

--- src/main/java/com/example/myapp/entity/Client.java
+++ src/main/java/com/example/myapp/entity/Client.java
@@ -1,4 +1,64 @@
 package com.example.myapp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "clients")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String tel;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public Client() {
+    }
+
+    public Client(int id, String name, String email, String tel) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.tel = tel;
+    }
 }
